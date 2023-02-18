c6c56c48f568eb3fb190528fe6002cf315bdf784
Migheli <avanesyan.migel@gmail.com>
2023-02-18 17:04:09 +0300

    refactor code

--- main.py
+++ main.py
@@ -7,87 +7,85 @@ from pathvalidate import sanitize_filename
 from requests import HTTPError
 from urllib.parse import unquote, urljoin, urlsplit
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-
-
 
 def check_for_redirect(response):
     if response.history:
         raise HTTPError
 
-#MAIN_PAGE_URL = 'https://tululu.org/'
 
-def get_filename(book_url, book_id):
-    response = requests.get(book_url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
+def parse_book_page(html_content):
+
+    soup = BeautifulSoup(html_content.text, 'lxml')
     title_tag = soup.find('td', class_='ow_px_td').find('h1')
     title_text, author = title_tag.text.split('::')
-    striped_title_text = title_text.strip()
+    title = title_text.strip()
 
-    return f'{book_id}. {striped_title_text}.txt'
+    genres_span = soup.find('span', class_='d_book')
+    genres_tags = genres_span.find_all('a')
+    genre_names = [genre.text for genre in genres_tags]
+
+    img_src = soup.find('div', class_='bookimage').find('img')['src']
+
+    comment_tags = soup.find_all('div', class_='texts')
+    comments = None
+    if comment_tags:
+        comments = [comment.find('span', class_='black').text for comment in comment_tags]
+
+    return {
+                'title': title,
+                'genres': genre_names,
+                'img_src': img_src,
+                'comments': comments,
+            }
 
 
-
-def download_txt(url, filename, folder='books/'):
-
-    sanitized_filename = f'{sanitize_filename(filename)}.txt'
+def download_txt(title, id, folder='books/'):
+    url = f'https://tululu.org/txt.php?id={id}'
+    sanitized_filename = f'{id} {sanitize_filename(title)}.txt'
     path = os.path.join(folder, sanitized_filename)
     response = requests.get(url, verify=False)
     response.raise_for_status()
-
+    check_for_redirect(response)
     with open(path, 'w') as file:
         file.write(response.text)
-    return path
 
-def download_img(book_url, folder='img/'):
 
-    response = requests.get(book_url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    img_src = soup.find('div', class_='bookimage').find('img')['src']
-
-    img_url = urljoin(book_url, img_src)
+def download_img(img_url, folder='img/'):
 
     response = requests.get(img_url)
     response.raise_for_status()
-
     sanitized_filename = unquote(urlsplit(img_url).path)
-    sanitized_filename = os.path.basename(sanitized_filename)
-
-
-    path = os.path.join(folder, sanitized_filename)
-    #comments = soup.find_all('div', class_='texts')
-    #if comments:
-    #    for comment in comments:
-    #        comment_text = comment.find('span', class_='black').text
-    #        print(comment_text)
-
-    genres = soup.find('span', class_='d_book')
-    genres = genres.find_all('a')
-    for genre in genres:
-        genre_name = genre.text
-        print(genre_name)
+    sanitized_filepath = os.path.basename(sanitized_filename)
+    path = os.path.join(folder, sanitized_filepath)
 
     with open(path, 'wb') as file:
         file.write(response.content)
-    return path
 
 
-os.makedirs(('books'),  exist_ok=True)
-os.makedirs(('img'),  exist_ok=True)
+def main():
 
-for id in range(1, 11):
-    book_file_url = f'https://tululu.org/txt.php?id={id}'
-    book_description_url = f'https://tululu.org/b{id}'
-    file_response = requests.get(book_file_url, verify=False)
-    description_response = requests.get(book_description_url, verify=False)
-    try:
-        check_for_redirect(file_response)
-        description_response.raise_for_status()
-        download_txt(book_file_url, get_filename(book_description_url, id))
-        download_img(book_description_url)
-    except HTTPError:
-        print('АЯ-ЯЙ! Ошибка')
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    os.makedirs(('books'), exist_ok=True)
+    os.makedirs(('img'), exist_ok=True)
 
+    for id in range(1, 11):
+        try:
+            book_url = f'https://tululu.org/b{id}/'
+            response = requests.get(book_url, verify=False)
+            response.raise_for_status()
+            print(response.history)
+            check_for_redirect(response)
+            book_info = parse_book_page(response)
+            img_url = urljoin(book_url, book_info['img_src'])
+            download_img(img_url)
+            download_txt(book_info['title'], id)
+            print(f'Скачана книга - {book_info["title"]} ')
+            print(f'Жанр: {book_info["genres"]}')
+            print(f'Комментарии к книге: {book_info["comments"]}')
+
+        except HTTPError:
+            print(f'Ошибка при скачивании книги с id {id}. Пропускаем.')
+
+
+if __name__ == "__main__":
+    main()
